af0cbd175cb012fe6e7f7196590e8637bacb926d
major173 <3321944339@qq.com>
2026-05-27 14:22:22 +0800

    新增同步环控制

--- APP/lift_task/lift_task.cpp
+++ APP/lift_task/lift_task.cpp
@@ -30,12 +30,23 @@ static TypedTopicSubscriber<pub_lift_cmd> lift_cmd_sub("lift_cmd", 8);
 static pub_lift_cmd lift_cmd{};
 
 float lift_2006_speed = 0.0f;
-float lift_3508_speed = 0.0f;
 float lift_3508_target_pos = 0.0f;
-float lift_3508_current_pos = 0.0f;
 float lift_3508_pos_pid_out = 0.0f;
 bool lift_3508_hold_enable = false;
 bool lift_3508_manual_last = false;
+
+float lift_3508_motor1_pos = 0.0f;
+float lift_3508_motor2_pos = 0.0f;
+float lift_3508_motor1_speed = 0.0f;
+float lift_3508_motor2_speed = 0.0f;
+
+float lift_3508_avg_pos = 0.0f;
+float lift_3508_diff_pos = 0.0f;
+
+float lift_3508_base_speed = 0.0f;
+float lift_3508_sync_pid_out = 0.0f;
+float lift_3508_motor1_ref_speed = 0.0f;
+float lift_3508_motor2_ref_speed = 0.0f;
 
 float lift_2006_motor1_pid_out = 0.0f;
 float lift_2006_motor2_pid_out = 0.0f;
@@ -44,8 +55,10 @@ float lift_3508_motor2_pid_out = 0.0f;
 
 constexpr float MAX_LIFT_2006_SPEED = 600.0f;
 constexpr float MAX_LIFT_3508_SPEED = 120.0f;
+constexpr float MAX_LIFT_3508_SYNC_COMP = 30.0f;
 
-constexpr float LIFT_LOW_POS = 50.0f;
+
+constexpr float LIFT_LOW_POS = 250.0f;
 constexpr float LIFT_HIGH_POS = -560.0f;
 
 constexpr float LIFT_2006_MOTOR1_DIR = 1.0f;
@@ -63,18 +76,24 @@ PID_t lift_3508_motor1_pid = {
 PID_t lift_3508_motor2_pid = {
     .Kp = 100.0f, .Ki = 30.0f, .Kd = 0.3f, .MaxOut = 16000, .DeadBand = 0.1f, .Improve = NONE};
 PID_t lift_3508_pos_pid = {
-    .Kp = 8.0f, .Ki = 0.1f, .Kd = 0.0f, .MaxOut = MAX_LIFT_3508_SPEED, .DeadBand = 0.1f, .Improve = NONE};
-
+    .Kp = 9.0f, .Ki = 0.1f, .Kd = 0.0f, .MaxOut = MAX_LIFT_3508_SPEED, .DeadBand = 0.1f, .Improve = NONE};
+PID_t lift_3508_sync_pid = {
+    .Kp = 0.28f, .Ki = 0.0f, .Kd = 0.0f, .MaxOut = MAX_LIFT_3508_SYNC_COMP, .DeadBand = 0.1f, .Improve = NONE};
 static inline void liftInit(void) {
   PID_Init(&lift_2006_motor1_pid);
   PID_Init(&lift_2006_motor2_pid);
   PID_Init(&lift_3508_motor1_pid);
   PID_Init(&lift_3508_motor2_pid);
   PID_Init(&lift_3508_pos_pid);
+  PID_Init(&lift_3508_sync_pid);
 
-  lift_3508_current_pos =
-      (lift_3508_motor1.getCurrentSumPos() + lift_3508_motor2.getCurrentSumPos()) / 2.0f;
-  lift_3508_target_pos = LIFT_LOW_POS;
+  lift_3508_motor1_pos = lift_3508_motor1.getCurrentSumPos();
+  lift_3508_motor2_pos = lift_3508_motor2.getCurrentSumPos();
+  lift_3508_avg_pos = (lift_3508_motor1_pos + lift_3508_motor2_pos) / 2.0f;
+  lift_3508_diff_pos = lift_3508_motor1_pos - lift_3508_motor2_pos;
+
+  //lift_3508_target_pos = LIFT_LOW_POS;
+  lift_3508_target_pos = lift_3508_avg_pos;
   lift_3508_hold_enable = true;
   lift_3508_manual_last = false;
 
@@ -84,41 +103,56 @@ static inline void liftInit(void) {
 }
 
 static inline void Lift_Data_Process(void) {
+
   lift_2006_speed = lift_cmd.lift_2006_input * MAX_LIFT_2006_SPEED;
-  lift_3508_current_pos =
-      (lift_3508_motor1.getCurrentSumPos() + lift_3508_motor2.getCurrentSumPos()) / 2.0f;
+
+  lift_3508_motor1_pos = lift_3508_motor1.getCurrentSumPos();
+  lift_3508_motor2_pos = lift_3508_motor2.getCurrentSumPos();
+  lift_3508_motor1_speed = lift_3508_motor1.getRawCurrentSpeed();
+  lift_3508_motor2_speed = lift_3508_motor2.getRawCurrentSpeed();
+
+  lift_3508_avg_pos = (lift_3508_motor1_pos + lift_3508_motor2_pos) / 2.0f;
+  lift_3508_diff_pos = lift_3508_motor1_pos - lift_3508_motor2_pos;
+
+ 
   const bool lift_3508_manual_active =
-      (lift_cmd.lift_up && !lift_cmd.lift_down) ||
-      (lift_cmd.lift_down && !lift_cmd.lift_up);
+  (lift_cmd.lift_up && !lift_cmd.lift_down) ||
+  (lift_cmd.lift_down && !lift_cmd.lift_up);
 
   if (lift_cmd.lift_up && !lift_cmd.lift_down) {
-    lift_3508_speed = MAX_LIFT_3508_SPEED;
+    lift_3508_base_speed = MAX_LIFT_3508_SPEED;
     lift_3508_hold_enable = false;
   } else if (lift_cmd.lift_down && !lift_cmd.lift_up) {
-    lift_3508_speed = -MAX_LIFT_3508_SPEED;
+    lift_3508_base_speed = -MAX_LIFT_3508_SPEED;
     lift_3508_hold_enable = false;
   } else {
     if (lift_3508_manual_last) {
-      lift_3508_target_pos = lift_3508_current_pos;
+      lift_3508_target_pos = lift_3508_avg_pos;
       if (lift_3508_target_pos > LIFT_LOW_POS) {
         lift_3508_target_pos = LIFT_LOW_POS;
       }
       if (lift_3508_target_pos < LIFT_HIGH_POS) {
         lift_3508_target_pos = LIFT_HIGH_POS;
       }
-      PID_Init(&lift_3508_pos_pid);
+
+    PID_Init(&lift_3508_pos_pid);
+    PID_Init(&lift_3508_sync_pid);
       lift_3508_hold_enable = true;
     }
-
     if (lift_3508_hold_enable) {
       lift_3508_pos_pid_out = PID_Calculate(&lift_3508_pos_pid,
-                                            lift_3508_current_pos,
+                                            lift_3508_avg_pos,
                                             lift_3508_target_pos);
-      lift_3508_speed = lift_3508_pos_pid_out;
+      lift_3508_base_speed = lift_3508_pos_pid_out;
     } else {
-      lift_3508_speed = 0.0f;
+      lift_3508_base_speed = 0.0f;
     }
   }
+  lift_3508_sync_pid_out =
+      PID_Calculate(&lift_3508_sync_pid, lift_3508_diff_pos, 0.0f);
+//待调试
+  lift_3508_motor1_ref_speed = lift_3508_base_speed + lift_3508_sync_pid_out;
+  lift_3508_motor2_ref_speed = lift_3508_base_speed - lift_3508_sync_pid_out;
 
   lift_3508_manual_last = lift_3508_manual_active;
 }
@@ -132,8 +166,9 @@ void liftTask(void *argument) {
 
   for (;;) {
     if (lift_cmd_sub.TryGet(&lift_cmd)) {
-      Lift_Data_Process();
+      
     }
+    Lift_Data_Process();
 
     lift_2006_motor1_pid_out =
         PID_Calculate(&lift_2006_motor1_pid,
@@ -144,14 +179,14 @@ void liftTask(void *argument) {
                       lift_2006_motor2.getRawCurrentSpeed(),
                       lift_2006_speed * LIFT_2006_MOTOR2_DIR);
 
-    lift_3508_motor1_pid_out =
+        lift_3508_motor1_pid_out =
         PID_Calculate(&lift_3508_motor1_pid,
-                      lift_3508_motor1.getRawCurrentSpeed(),
-                      lift_3508_speed * LIFT_3508_MOTOR1_DIR);
+                      lift_3508_motor1_speed,
+                      lift_3508_motor1_ref_speed * LIFT_3508_MOTOR1_DIR);
     lift_3508_motor2_pid_out =
         PID_Calculate(&lift_3508_motor2_pid,
-                      lift_3508_motor2.getRawCurrentSpeed(),
-                      lift_3508_speed * LIFT_3508_MOTOR2_DIR);
+                      lift_3508_motor2_speed,
+                      lift_3508_motor2_ref_speed * LIFT_3508_MOTOR2_DIR);
 
     lift_2006_motor1.setMotorCmd(lift_2006_motor1_pid_out);
     lift_2006_motor2.setMotorCmd(lift_2006_motor2_pid_out);
