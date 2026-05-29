fb3acf24cb4d6bc27478d5d8d721b9c3b50c3ca7
Howie41 <fish_howie41@qq.com>
2026-05-29 14:45:28 +0800

    feat: 新增武馆部分状态和工具函数

--- APP/state_machine_task/state_machine_task.cpp
+++ APP/state_machine_task/state_machine_task.cpp
@@ -6,13 +6,73 @@
  */
 
 #include "state_machine_task.h"
+#include "cmsis_os2.h"
 #include <atomic>
 osThreadId_t StateMachineTaskHandle;
 
-
+static std::atomic<RobotState> current_state{RobotState::begin};
 
 void stateMachineTask(void *argument) {
     for (;;) {
+        switch (current_state.load()) {
+
+            case RobotState::begin:
+                break;
+
+            case RobotState::go_to_SHR:
+                break;
+
+            case RobotState::aim_at_weapon:
+                break;
+
+            case RobotState::catch_weapon:
+                break;
+
+            case RobotState::rotate_weapon_claw:
+                break;
+
+            case RobotState::wait_for_cmd:
+                break;
+
+            default:
+                // 不应该到达这里
+                break;
+        }
         osDelay(1);
     }
+}
+
+/**
+ * @brief 等待直到条件满足
+ * @param condition 条件函数，传一个匿名函数就行，返回布尔值
+ * @param delay_ms 多久检查一次条件
+ */
+template <typename T>
+void wait_until(T &&condition, uint32_t delay_ms = 100U) {
+    while (!condition()) {
+        osDelay(delay_ms);
+    }
+}
+
+/**
+ * @brief 等待直到条件满足或超时
+ * @param condition 条件函数，传一个匿名函数就行，返回布尔值
+ * @param timeout_ms 超时时间
+ * @param delay_ms 多久检查一次条件
+ * @return true 条件满足，false 超时
+ */
+template <typename T>
+bool wait_until_timeout_or(T &&condition, uint32_t timeout_ms, uint32_t delay_ms = 100U) {
+    const uint32_t start = osKernelGetTickCount();
+    while (!condition()) {
+        if ((osKernelGetTickCount() - start) >= timeout_ms) {
+            return false;
+        }
+        osDelay(delay_ms);
+    }
+    return true;
+}
+
+void change_state_to(RobotState new_state) {
+    current_state.store(new_state);
 }
--- APP/state_machine_task/state_machine_task.h
+++ APP/state_machine_task/state_machine_task.h
@@ -12,9 +12,17 @@ extern osThreadId_t StateMachineTaskHandle;
 
 void stateMachineTask(void *argument);
 
+#ifdef __cplusplus
 // SpearheadRack - SHR 端头架
 // StaffRack - SR 长杆架
-enum class RobotState {
-    READY,
-    MOVE_TO_SHR,
+enum class RobotState: uint8_t {
+    // 武馆
+    begin = 0,            // 启动
+    go_to_SHR,            // 前往端头架
+    aim_at_weapon,        // 夹爪对准对应武器头
+    catch_weapon,         // 夹爪夹取武器
+    rotate_weapon_claw,   // 夹爪反转
+    wait_for_cmd          // 等待R1指令
+    // 梅林
 };
+#endif // __cplusplus
